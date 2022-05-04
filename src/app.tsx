c74480c1b7e2137fb3fff0dc2934534cb3c692db
Moustafa moungari <moungari.moustafa@gmail.com>
2022-05-04 13:40:09 +0200

    creation et affichage du nombre de pokemons sur notre page

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,8 @@
 
 /* ========  destructuring d'ecmaScript6   =============*/
 import React,{FunctionComponent,useState} from 'react'; 
+import POKEMONS from './models/mock-pokemon';
+import Pokemon from './models/pokemon';
 
 /* ======== useState import du Hook pour pour utiliser les states dans un composant de function ========= */
 
@@ -15,10 +17,29 @@ const App: FunctionComponent = () => {
 /* ==== le Hooks useState renvoie un tableau de 2 elements le SetName permet de gerer l'etat  
 nous creons une variable name est nous l'initialisons avec la variable React
 ===*/
-const[name,SetName] = useState<String>('React'); // on definit un type les données String sauvegarder dans notre state
+
+
+/* ==== 3 etapes de la vie d'un composant 
+1) la creation -> componentDidMount() insertion dans le dom 'Montage du composant'
+2) modification 'componentDidUpdate(prevProps,prevState) recoit les props et state avant la mise a jour du composant
+3) suppression d'un composant 'componentWillUnmount le composant est detruit lorsqu'il est retiré du DOM navigation de l'utilisateur par exemple
+ceci permet de desabonner certaines dependance du composant. et ainsi eviter les problemes de performances. 'Demontage'
+===*/
+const[title,SetName] = useState<String>('POKEMONS'); // on definit un type les données String sauvegarder dans notre state
+const[pokemons] = useState<Pokemon[]>(POKEMONS);
+
+
+
     
  return (
-  <h1>Hello, {name} !</h1>  /* ==== utilisation de javascript dans jsx. ===*/
+   
+   <div>
+     <div>
+     <p><h2>NOMBRE DE {title} CONTENU DANS LA BASE :</h2></p>
+   </div>
+    <p><h3>il y a  {pokemons.length} Pokemons dans votre liste. !</h3> </p> 
+   </div>
+ 
  )
 }
   
