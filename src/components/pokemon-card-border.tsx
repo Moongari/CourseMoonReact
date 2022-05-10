3473d7289d7a1695f2774195bd2583ad37a8d59c
Moustafa moungari <moungari.moustafa@gmail.com>
2022-05-10 09:34:50 +0200

    ajout d'un pokemonCardBorder

---
+++ src/components/pokemon-card-border.tsx
@@ -0,0 +1,48 @@
+import React,{FunctionComponent, useState} from "react";
+import Pokemon from "../models/pokemon";
+import './pokemon-card.css';
+
+//on definit un type via typeScript pour etre sur de ne passer qu'une props de type Pokemon
+type Props ={
+    pokemon:Pokemon,
+    borderColor?: string //variable facultative
+};
+
+
+const PokemonCardBorder : FunctionComponent<Props> =({pokemon,borderColor="gray"})=>{
+
+    const [color,setColor] = useState<string>();
+
+    const showBorder= ()=>{
+        setColor("red");
+    }
+
+    const hideBorder= ()=>{
+        setColor("gray");
+    }
+    return(
+        <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
+            
+          <div className="card horizontal" style={{borderColor:color}}>
+            <div className="card-image"> 
+              <img src={pokemon.picture} alt={pokemon.name}/>
+            </div>
+            <div className="card-stacked">
+              <div className="card-content">
+                <p>{pokemon.name}</p>
+                <p><small>{pokemon.created.toString()}</small></p>
+              </div>
+            </div>
+          </div> 
+        </div>
+    
+        );
+
+
+}
+
+
+export default PokemonCardBorder;
+
+
+
--- src/pages/pokemon-list.tsx
+++ src/pages/pokemon-list.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from '../models/pokemon';
 import POKEMONS from '../models/mock-pokemon';
 import PokemonCard from '../components/pokemon-card';
+import PokemonCardBorder from '../components/pokemon-card-border';
   
 const PokemonList: FunctionComponent = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
