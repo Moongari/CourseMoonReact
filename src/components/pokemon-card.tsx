6554018c36fd76c98b674edfec2469e2c3b9ed1b
Moustafa moungari <moungari.moustafa@gmail.com>
2022-05-10 10:02:25 +0200

    Factorisation des comportements afin de pouvoir les reutiliser plus facilement formatDate et FormatType

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,4 +1,6 @@
 import React,{FunctionComponent, useState} from "react";
+import formatDate from "../helpers/format-date";
+import formatType from "../helpers/format-Type";
 import Pokemon from "../models/pokemon";
 import './pokemon-card.css';
 
@@ -28,9 +30,55 @@ const PokemonCard : FunctionComponent<Props>=({pokemon,backgroundcolor="gray"})=
         setColor("white"); // on remet la couleur white
     }
 
-    const formatDate = (date: Date): string =>{
-      return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-    }
+    // const formatDate = (date: Date): string =>{
+    //   return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
+    // }
+
+    // const formatType = (type: string): string => {
+    //   let color: string;
+     
+    //   switch (type) {
+    //     case 'Feu': 
+    //       color = 'red lighten-1'; 
+    //       break; 
+    //     case 'Eau': 
+    //       color = 'blue lighten-1'; 
+    //       break; 
+    //     case 'Plante': 
+    //       color = 'green lighten-1'; 
+    //       break; 
+    //     case 'Insecte': 
+    //       color = 'brown lighten-1'; 
+    //       break; 
+    //     case 'Normal': 
+    //       color = 'grey lighten-3'; 
+    //       break; 
+    //     case 'Vol': 
+    //       color = 'blue lighten-3'; 
+    //       break; 
+    //     case 'Poison': 
+    //       color = 'deep-purple accent-1'; 
+    //       break; 
+    //     case 'Fée': 
+    //       color = 'pink lighten-4'; 
+    //       break; 
+    //     case 'Psy': 
+    //       color = 'deep-purple darken-2'; 
+    //       break; 
+    //     case 'Electrik': 
+    //       color = 'lime accent-1'; 
+    //       break; 
+    //     case 'Combat': 
+    //       color = 'deep-orange'; 
+    //       break; 
+    //     default: 
+    //       color = 'grey'; 
+    //       break; 
+    //   }
+     
+    //   return `chip ${color}`;
+    // }
+    
 
     return(
     <div className="col s6 m4" onMouseEnter={showBorder} onMouseLeave={hideBorder}>
@@ -43,6 +91,10 @@ const PokemonCard : FunctionComponent<Props>=({pokemon,backgroundcolor="gray"})=
           <div className="card-content">
             <p>{pokemon.name}</p>
             <p><small>{formatDate(pokemon.created)}</small></p>
+
+            {pokemon.types.map(type =>(
+              <span key={type} className={formatType(type)}>{type}</span>
+            ))}
           
           </div>
         </div>
